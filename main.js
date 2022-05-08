ee9f76aac61daee0c9b82296d91b49c5a44d11d5
XThiagoX <thiagoaraujo43034@gmail.com>
2022-05-08 20:36:52 -0300

    criação de button 'Back to Top' orientação em JS

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
+window.addEventListener('scroll', onScroll)
+onScroll()
+
 function onScroll()
 {
+  showNavOnScroll()
+  showButtonBackToTop()
+}
+
+function showNavOnScroll(){
   if(scrollY > 0){
     navigation.classList.add('scroll')
   }
   else{
     navigation.classList.remove('scroll')
+  }
+}
+
+function showButtonBackToTop(){
+  if(scrollY > 1900){
+    backToTopButton.classList.add('show')
+  }
+  else{
+    backToTopButton.classList.remove('show')
   }
 }
 
@@ -29,4 +46,5 @@ ScrollReveal({
 #about,
 #about header,
 #about content`);
+
 
